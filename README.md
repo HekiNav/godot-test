b049b173c98370621fc01dd41aff269ca401c2bd
HekiNav <foobagames@gmail.com>
2025-09-23 13:56:02 +0300

    actually merged

--- README.md
+++ README.md
@@ -1,6 +1,2 @@
-<<<<<<< HEAD
 # game
 [todo]: do readme
-=======
-# godot test
->>>>>>> a314cc22ba35af738349faf9b38475bc5ae429bb
--- game.gd
+++ game.gd
@@ -4,6 +4,6 @@ const BOARD = preload("uid://cp3h0in414hf7")
 
 func _ready():
 	var board = BOARD.instantiate()
-	board.size = 2
+	board.size = 8
 	board.gap = 0.2	
 	add_child(board)
